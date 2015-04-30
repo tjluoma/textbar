39db7725f322e3e1c8f2ff1d5828415417064d2c
TJ Luoma <luomat@gmail.com>
2015-04-30 16:48:26 -0400

    use `find` instead of `mount`

--- textbar-mount-count.sh
+++ textbar-mount-count.sh
@@ -6,19 +6,16 @@
 # Web: 	http://RhymesWithDiploma.com
 # Date:	2015-04-18
 
-COUNT=`/sbin/mount | /usr/bin/egrep -v '^(devfs|map) ' | /usr/bin/wc -l | /usr/bin/tr -dc '[0-9]'`
-
-	# We don't want to count the boot drive, so subtract 1
-COUNT=$(($COUNT - 1))
+COUNT=`find /Volumes/* -maxdepth 0 -type d -print | wc -l | tr -dc '[0-9]'`
 
 if [ "$COUNT" = "0" ]
 then
-	echo -n "No Mounts"
+	echo "No Mounts"
 elif [ "$COUNT" = "1" ]
 then
-	echo -n "1 Mount"
+	echo "1 Mount"
 else
-	echo -n "$COUNT Mounts"
+	echo "$COUNT Mounts"
 fi
 
 exit 0
